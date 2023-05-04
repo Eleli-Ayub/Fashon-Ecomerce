d7fed06ae53523313dd87934065a91b52cf40dbc
Eleli-Ayub <lesosioayub@gmail.com>
2023-05-04 03:37:22 +0300

    Innitialized web commerce for fashion

---
+++ src/data.js
@@ -0,0 +1,74 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://www.searchenginejournal.com/wp-content/uploads/2022/08/google-shopping-ads-6304dccb7a49e-sej.png",
+    title: "SUMMER SALE",
+    desc: " DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+    bg: "f5fafd",
+  },
+  {
+    id: 1,
+    img: "https://indian-retailer.s3.ap-south-1.amazonaws.com/s3fs-public/2021-05/revenge%20shopping.jpg",
+    title: "WINTER SALE",
+    desc: " DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+    bg: "fcf1ed",
+  },
+  {
+    id: 1,
+    img: "https://joburg.co.za/wp-content/uploads/2020/08/641.jpg",
+    title: "AUTUMN COLLECTION",
+    desc: " DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+    bg: "fbf0f4",
+  },
+];
+export const categories = [
+  {
+    id: 1,
+    img: "https://cdn.shopify.com/s/files/1/0692/4255/files/S_L_NavyTux_WARM_1_1_1_x800.jpg?v=1681049254",
+    title: "SHIRT STYLE",
+  },
+  {
+    id: 1,
+    img: "https://cdn.shopify.com/s/files/1/0418/5309/1992/products/AmraFashionValentine_sDayLoveHeartPrintLongSleevesTwo-pieceLoungewear_800x.jpg?v=1649636544",
+    title: "LOUNGEWEAR LOVE",
+  },
+  {
+    id: 1,
+    img: "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1651860048-download-14-1651860041.jpg?crop=0.8884615384615384xw:1xh;center,top&resize=980:*",
+    title: "LIGHT JACKETS",
+  },
+];
+export const popularProducts = [
+  {
+    id: 1,
+    img: "https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+  },
+  {
+    id: 2,
+    img: "https://static.fibre2fashion.com/MemberResources/LeadResources/1/2013/9/Seller/1347469/Images/1347469_0_201309221024030000000.jpg",
+  },
+  {
+    id: 3,
+    img: "https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+  },
+  {
+    id: 4,
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Christmas_Sweater.jpg/640px-Christmas_Sweater.jpg",
+  },
+  {
+    id: 5,
+    img: "https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+  },
+  {
+    id: 6,
+    img: "https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+  },
+  {
+    id: 7,
+    img: "https://cdn.shopify.com/s/files/1/0056/2583/9650/products/PA-22SU403_M.grey_main_800x1000_800x1000_d94c0b93-721f-4a48-b6ae-13e33a274da2_1024x1024.jpg?v=1678211131",
+  },
+  {
+    id: 8,
+    img: "https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+  },
+];
